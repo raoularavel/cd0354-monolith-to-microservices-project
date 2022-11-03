f6f589aff4e9a7994c285d4ab27a97d837d3bf6d
raoularavel <rkuate1050@gmail.com>
2022-11-03 04:49:48 +0100

    Add access control origin to proxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,11 +3,3 @@
   appName: 'Udagram',
   apiHost: `http://a3bcd86b9895545a6b6b0905214c54d5-1273087818.us-east-2.elb.amazonaws.com:8080/api/v0`
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
