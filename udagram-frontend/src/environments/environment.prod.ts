efd0ed73c18b7df5bb4bdf07395024593f1b7b2d
raoularavel <rkuate1050@gmail.com>
2022-11-04 04:45:42 +0100

    Proxy url updated

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: `http://a0e4d0a6141ed4cf4948c2044e7a3bb7-1819728608.us-east-2.elb.amazonaws.com:8080/api/v0`
+  apiHost: "http://a53919a584a6b4c9b8fbb7470d5f5bfe-378152442.us-east-2.elb.amazonaws.com:8080/api/v0"
 };
 
 
