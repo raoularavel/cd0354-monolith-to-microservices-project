d1ea134887a4ce096f694bc2df818110cc61a6ea
raoularavel <rkuate1050@gmail.com>
2022-11-04 05:11:32 +0100

    Proxy url updated in  frontend pod

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
  export const environment = {
   production: false,
-  appName: 'Udagram',
+  appName: 'Udagram Raoul KUATE',
   apiHost: "http://a53919a584a6b4c9b8fbb7470d5f5bfe-378152442.us-east-2.elb.amazonaws.com:8080/api/v0"
 };
