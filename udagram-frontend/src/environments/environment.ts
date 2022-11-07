104bb709ce6b1ac918c38cc8edfe0fc22b563353
raoularavel <rkuate1050@gmail.com>
2022-11-07 04:24:03 +0100

    Proxy url updated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
  export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram Raoul KUATE',
   apiHost: "http://ae0ecf7ac64be4ba5a0d947387f705f5-1808537725.us-east-2.elb.amazonaws.com:8080/api/v0"
 };
